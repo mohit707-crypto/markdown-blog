7bae4c084a77c69dd89e0f5ef251ebc1a64744d6
mohit707-crypto <mohit_khandar@yahoo.com>
2020-08-05 14:37:06 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const Article = require('./models/aticles')
+const articleRouter = require('./routes/articles')
+const methodOverride = require('method-override')
+const app = express()
+
+mongoose.connect('mongodb://localhost/blog', { useNewUrlParser: true ,  useUnifiedTopology: true, usecreateIndexe:true })
+
+app.set('view engine','ejs')
+app.use(express.urlencoded({ extended: false}))
+app.use(methodOverride('_method'))
+
+
+app.get('/', async (req, res)=>{  
+  const articles = await Article.find().sort({ createdAt: 'desc' })
+    res.render('articles/index', {articles: articles })
+})
+
+
+app.use('/articles', articleRouter)
+app.listen(5000, () => {
+    console.log("server is runnning on 5000")
+})
